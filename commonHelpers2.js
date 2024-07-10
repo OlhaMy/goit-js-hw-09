ccc9c1c931e23ea66e2145ace3b3947f9d52937d
OlhaMy <124881449+OlhaMy@users.noreply.github.com>
2024-07-10 13:52:45 +0000

    Deploying to gh-pages from @ OlhaMy/goit-js-hw-09@84e78069cd4b897d85cbf9dc5751f4f6ba9e297f 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const a=document.querySelector(".feedback-form");a.addEventListener("submit",e=>{e.preventDefault();const t=e.target.element.email.value.trim(),l=e.target.element.message.value.trim();(!t||!l)&&alert("Fill please all fields")});
+//# sourceMappingURL=commonHelpers2.js.map
